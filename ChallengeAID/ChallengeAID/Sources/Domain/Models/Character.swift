96f4e3a43fe66f6af08c41248953170c9e582c34
Wilton Ramos <wilton.silva@bancointer.com.br>
2022-07-02 20:27:13 -0300

    Creates Character and Comic Codable

--- ChallengeAID/ChallengeAID/Sources/Domain/Models/Character.swift
+++ ChallengeAID/ChallengeAID/Sources/Domain/Models/Character.swift
@@ -6,3 +6,101 @@
 //
 
 import Foundation
+
+struct CharacterResponse: Codable {
+    let code: Int?
+    let status: String?
+    let copyright: String?
+    let attributionText: String?
+    let attributionHTML: String?
+    let data: CharacterDataContainer?
+    let etag: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case code
+        case status
+        case copyright
+        case attributionText
+        case attributionHTML
+        case data
+        case etag
+    }
+}
+
+struct CharacterDataContainer: Codable {
+    let offset: Int?
+    let limit: Int?
+    let total: Int?
+    let count: Int?
+    let results: [Character]?
+    
+    enum CodingKeys: String, CodingKey {
+        case offset
+        case limit
+        case total
+        case count
+        case results
+    }
+}
+
+struct Character: Codable {
+    let id: Int
+    let name, description: String
+    let modified: Date
+    let thumbnail: Thumbnail
+    let resourceURI: String
+    let comics, series: Comics
+    let stories: Stories
+    let events: Comics
+    let urls: [URLElement]
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, description
+        case modified, thumbnail, resourceURI, comics, series, stories, events, urls
+    }
+}
+
+struct Comics: Codable {
+    let available: Int
+    let collectionURI: String
+    let items: [ComicsItem]
+    let returned: Int
+}
+
+struct ComicsItem: Codable {
+    let resourceURI: String
+    let name: String
+}
+
+struct Stories: Codable {
+    let available: Int
+    let collectionURI: String
+    let items: [StoriesItem]
+    let returned: Int
+}
+
+struct StoriesItem: Codable {
+    let resourceURI: String
+    let name: String
+    let type: TypeEnum
+}
+
+enum TypeEnum: String, Codable {
+    case cover = "cover"
+    case interiorStory = "interiorStory"
+}
+
+struct Thumbnail: Codable {
+    let path: String
+    let thumbnailExtension: String
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
+struct URLElement: Codable {
+    let type: String
+    let url: String
+}
--- ChallengeAID/ChallengeAID/Sources/Domain/Models/Comic.swift
+++ ChallengeAID/ChallengeAID/Sources/Domain/Models/Comic.swift
@@ -6,3 +6,79 @@
 //
 
 import Foundation
+
+struct ComicResponse: Codable {
+    let code: Int?
+    let status: String?
+    let copyright: String?
+    let attributionText: String?
+    let attributionHTML: String?
+    let data: ComicDataContainer?
+    let etag: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case code
+        case status
+        case copyright
+        case attributionText
+        case attributionHTML
+        case data
+        case etag
+    }
+}
+
+struct ComicDataContainer: Codable {
+    let offset: Int?
+    let limit: Int?
+    let total: Int?
+    let count: Int?
+    let results: [Comic]?
+    
+    enum CodingKeys: String, CodingKey {
+        case offset
+        case limit
+        case total
+        case count
+        case results
+    }
+}
+
+struct Comic: Codable {
+    let id: Int?
+    let digitalId: Int?
+    let title: String?
+    let images: [Image]?
+    let characters: CharacterList?
+    let description: String?
+    let creators: CreatorList?
+}
+
+struct Image: Codable {
+    let path: String?
+    let imageExtension: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case path
+        case imageExtension = "extension"
+    }
+}
+
+struct CreatorList: Codable {
+    let available: Int?
+    let returned: Int?
+    let collectionURI: String?
+    let items: [CharacterSummary]?
+}
+
+struct CharacterList: Codable {
+    let available: Int?
+    let returned: Int?
+    let collectionURI: String?
+    let items: [CharacterSummary]?
+}
+
+struct CharacterSummary: Codable {
+    let resourceURI: String?
+    let name: String?
+    let role: String?
+}
